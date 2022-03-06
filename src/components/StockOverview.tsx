9a791e124531f1c801af0afafc59ea04e9294895
ethan <fethanerrier@gmail.com>
2022-03-05 21:56:24 -0800

    connect to new express verison which returns userdata and stockdata in an object instead of an array

--- src/components/StockOverview.tsx
+++ src/components/StockOverview.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import axios from 'axios';
-import { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonBadge, IonList, IonText, IonAvatar, IonThumbnail, IonButton, IonIcon, IonDatetime, IonSelect, IonSelectOption, IonToggle, IonInput, IonCheckbox, IonRange, IonNote } from '@ionic/react';
-import { closeCircle, home, star, navigate, informationCircle, checkmarkCircle, shuffle, thumbsUpOutline, arrowUp, arrowDown,  } from 'ionicons/icons';
+import React from "react";
+import { IonItem, IonLabel, IonBadge } from "@ionic/react";
+import { shuffle, thumbsUpOutline, arrowUp, arrowDown } from "ionicons/icons";
 
 interface Props {
-    stock: {
+  stock: {
+    StockData: {
       symbol: string;
       marketOpen: number;
       marketClose: number;
@@ -15,55 +14,59 @@ interface Props {
       revenue: number;
       dividendsPerShare: number;
     };
-    i: number;
+    userStockData: {
+      symbol: string;
+      toHighPrice: number;
+      highPrice: number;
+      lowPrice: number;
+      toLowPrice: number;
+    };
+  };
+  i: number;
 }
 
-let Actions: Array<string> = ['Buy', 'Sell', 'Hold'];
+let Actions: Array<string> = ["Buy", "Sell", "Hold"];
 
-
-const StockOverview: React.FC<Props> = ({stock, i}) => {
-
-// nFormatter is a funcion used to take a large Number(num) and a digits variable. the 
-// digits variable (0 or 1) decieds if you want a decimal piont or not
+const StockOverview: React.FC<Props> = ({ stock, i }) => {
+  // nFormatter is a funcion used to take a large Number(num) and a digits variable. the
+  // digits variable (0 or 1) decieds if you want a decimal piont or not
   function nFormatter(num: number, digits: number) {
-    const lookup = [  
+    const lookup = [
       { value: 1, symbol: "" },
       { value: 1e3, symbol: "k" },
       { value: 1e6, symbol: "M" },
       { value: 1e9, symbol: "B" },
       { value: 1e12, symbol: "T" },
       { value: 1e15, symbol: "P" },
-      { value: 1e18, symbol: "E" }
+      { value: 1e18, symbol: "E" },
     ];
     const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    var item = lookup.slice().reverse().find(function(item) {
-      return num >= item.value;
-    });
-    return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+    var item = lookup
+      .slice()
+      .reverse()
+      .find(function (item) {
+        return num >= item.value;
+      });
+    return item
+      ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol
+      : "0";
   }
 
-  // change the large numbers form the stock data into understanable numbers, eg 126B 
-  let formattedMarketCap = nFormatter(stock.marketCap, 0)
-  let formattedRevenue = nFormatter(stock.revenue, 0)
+  // change the large numbers form the stock data into understanable numbers, eg 126B
+  let formattedMarketCap = nFormatter(stock.StockData.marketCap, 0);
+  let formattedRevenue = nFormatter(stock.StockData.revenue, 0);
 
-
-    return (
- 
-        
-       <div className='row' key={stock.symbol} > 
-        <IonItem >
-          {/* <IonBadge color={stock.Color}> */}
-          <IonBadge>
-            {formattedRevenue}
-          </IonBadge>
-          {/* <ion-icon name="thumbs-up-sharp"></ion-icon> */}
-          {/* <IonIcon icon={arrowUp} /> */}
-          <IonLabel class="ion-margin-start"> {stock.symbol} </IonLabel>
-          <IonItem slot="end">
-          ${formattedMarketCap}
-          </IonItem>
-        </IonItem>
-      </div>
+  return (
+    <div className="row" key={stock.StockData.symbol}>
+      <IonItem>
+        {/* <IonBadge color={stock.Color}> */}
+        <IonBadge>{formattedRevenue}</IonBadge>
+        {/* <ion-icon name="thumbs-up-sharp"></ion-icon> */}
+        {/* <IonIcon icon={arrowUp} /> */}
+        <IonLabel class="ion-margin-start"> {stock.StockData.symbol} </IonLabel>
+        <IonItem slot="end">${formattedMarketCap}</IonItem>
+      </IonItem>
+    </div>
   );
 };
 
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,74 +1,102 @@
-import { useState, useEffect } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import StockOverview from '../components/StockOverview';
+import { useState, useEffect } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import StockOverview from "../components/StockOverview";
 
-import './Tab2.css';
+import "./Tab2.css";
 
-
+// let stockObject = {
+//   StockData: {
+//     symbol: stockData.symbol,
+//     marketOpen: stockData.regularMarketOpen,
+//     marketClose: stockData.regularMarketPreviousClose,
+//     sharesShort: stockData.sharesShort,
+//     totalCash: stockData.totalCash,
+//     marketCap: stockData.marketCap,
+//     revenue: stockData.revenue,
+//     dividendsPerShare: stockData.dividendsPerShare,
+//   },
+//   userStockData: {
+//     symbol: userStockData[0].symbol,
+//     toHighPrice: userStockData[0].toHighPrice,
+//     highPrice: userStockData[0].highPrice,
+//     lowPrice: userStockData[0].lowPrice,
+//     toLowPrice: userStockData[0].toLowPrice,
+//   },
+// };
 
 interface listItems {
-  symbol: string;
-  marketOpen: number;
-  marketClose: number;
-  sharesShort: number;
-  totalCash: number;
-  marketCap: number;
-  revenue: number;
-  dividendsPerShare: number;
+  StockData: {
+    symbol: string;
+    marketOpen: number;
+    marketClose: number;
+    sharesShort: number;
+    totalCash: number;
+    marketCap: number;
+    revenue: number;
+    dividendsPerShare: number;
+  };
+  userStockData: {
+    symbol: string;
+    toHighPrice: number;
+    highPrice: number;
+    lowPrice: number;
+    toLowPrice: number;
+  };
 }
 
-interface stockInfo extends Array<listItems>{}; 
+interface stockInfo extends Array<listItems> {}
 
 interface ContainerProps {
   name: string;
 }
 
-// let StockInfo: Array<string> = ['TSLA' 
+// let StockInfo: Array<string> = ['TSLA'
 // , 'ARKK', 'LMND', 'FUV', "FL","ETH-USD", "DJI", "GSPC", "CARG", "IXIC", "RH", "CLF", "ISPO", "ABBV", "XRP-USD", "SE", "CM.TO", "REGI", "X", "ALF", "JNJ", "ZOM", "BTE.TO", "MULN", "FCX", "NUE", "CYDY", "XRP-CAD", "NKE", "TAL", "LMT", "WTRH"
 // ];
 // let Stockssssss: Array<string> = ["TSLA", 'AMD', "FUV"]
 
 const Tab2: React.FC = () => {
-  const [ stockInfo, setstockInfo ] = useState<any>([]);
+  const [stockInfo, setstockInfo] = useState<any>([]);
 
+  useEffect(() => {
+    sendRequest().then((data: any) => {
+      setstockInfo(data);
+    });
+  }, []);
 
+  // Yahoo finace API
+  // var options = {
+  //   method: 'GET',
+  //   url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+  //   params: {symbol: Stock, region: 'US'},
+  //   headers: {
+  //     'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
+  //     'x-rapidapi-key': '63a51f3279msh18e8edbbf2f8ab4p136b71jsnb9c98ca45e18'
+  //   }
+  // };
 
-      useEffect(() => {
-      sendRequest().then((data: any) => {
-              setstockInfo(data)
-      });
-      }, []);
+  var options = {
+    method: "GET",
+    url: "http://localhost:3000/",
+    params: { username: "fethanerrier" },
+  };
 
-      // Yahoo finace API 
-      // var options = {
-      //   method: 'GET',
-      //   url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
-      //   params: {symbol: Stock, region: 'US'},
-      //   headers: {
-      //     'x-rapidapi-host': 'yh-finance.p.rapidapi.com',
-      //     'x-rapidapi-key': '63a51f3279msh18e8edbbf2f8ab4p136b71jsnb9c98ca45e18'
-      //   }
-      // };
-
-      var options = {
-        method: 'GET',
-        url: 'http://localhost:3000/',
-        params: {stocks: ["TSLA", 'AMD', "FUV"]},
-       
-      };
-
-
-      const sendRequest = () => {
-      console.log("Sending Get request")
-      return axios.request(options).then((response: any) => {
+  const sendRequest = () => {
+    console.log("Sending Get request");
+    return axios.request(options).then((response: any) => {
       return response.data;
-      })
-      };
+    });
+  };
 
-
-    var axios = require("axios").default;
-    console.log(stockInfo);
+  var axios = require("axios").default;
+  console.log(stockInfo);
 
   return (
     <IonPage>
@@ -85,16 +113,10 @@ const Tab2: React.FC = () => {
         </IonHeader>
         <IonContent>
           {/*-- Default Item --*/}
-          
-          {
-          stockInfo.map((stock: any, i: number) => {
-            return(
-              <StockOverview stock={stock} i={i} /> 
-            )
-            })  
-          
-            }
-            
+
+          {stockInfo.map((stock: any, i: number) => {
+            return <StockOverview stock={stock} i={i} />;
+          })}
         </IonContent>
       </IonContent>
     </IonPage>
@@ -102,18 +124,3 @@ const Tab2: React.FC = () => {
 };
 
 export default Tab2;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
